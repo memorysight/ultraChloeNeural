a16f18c1bae28a0d153a79c9f70f02397ec116c2
Daniel Smith <memorysight@gmail.com>
2024-04-30 10:59:41 -0400

    need to completed map

---
+++ ultra-chloe-neural/src/server2.js
@@ -0,0 +1,49 @@
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+// require( 'dotenv').config()
+
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI('AIzaSyDKUIoWlpmTSWlFnDyJS_zJmcMgXbZg2Og');
+
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello, I have 2 dogs in my house." }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+    generationConfig: {
+      maxOutputTokens: 100,
+    },
+  });
+
+  const msg = "How many paws are in my house?";
+
+  const result = await chat.sendMessage(msg);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
