fdf6e5106d91e2a13c0ec0e727df953c60c4b3c7
Daniel Smith <memorysight@gmail.com>
2024-07-10 16:56:04 -0400

    added spectrumAnalyzer for microphone

--- ultra-chloe-neural/src/App.js
+++ ultra-chloe-neural/src/App.js
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import SpeechRecognition from 'react-speech-recognition';
 import Dictaphone from './Dictaphone';
 import VoiceToText from './VoiceToText';
+import AudioVisualizer from './AudioAnalyzer';
 
 const App = () => {
   const [value, setValue] = useState("");
@@ -153,6 +154,15 @@ const App = () => {
       </video>
       <Dictaphone utterQuestion={utterQuestion} />
       <VoiceToText />
+      
+    <div className="MicVisualizer">
+          <div className="controls">
+            <button onClick={this.toggleMicrophone}>
+              {this.state.audio ? 'Stop Microphone' : 'Get microphone input'}
+            </button>
+          </div>
+          {this.state.audio ? <AudioAnalyzer audio={this.state.audio} /> : ''}
+        </div>
 
       <p>Please ask a question:
         <button className="surprise" onClick={surprise} disabled={!chatHistory}>Surprise me</button>
