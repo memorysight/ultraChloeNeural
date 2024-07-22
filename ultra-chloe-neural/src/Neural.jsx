72f8084a766f2cf1ae59640041aadd1c9f78c9b8
Daniel Smith <memorysight@gmail.com>
2024-07-22 17:19:09 -0400

    created a better Jen

--- ultra-chloe-neural/src/Neural.jsx
+++ ultra-chloe-neural/src/Neural.jsx
@@ -147,7 +147,7 @@ const Neural = () => {
   return (
     <div className="app">
       <video autoPlay muted loop id="bg-video">
-        <source src="jenxxxtalkingdONE5.mp4" type="video/mp4" />
+        <source src="JenLips.mp4" type="video/mp4" />
       </video>
       <Dictaphone utterQuestion={utterQuestion} />
       <VoiceToText />
