9ad4d5b927cf1ce16fd50f90d918192671ba78ee
Daniel Smith <memorysight@gmail.com>
2024-07-29 19:31:40 -0400

    added angry Zoe

--- ultra-chloe-neural/src/Neural.jsx
+++ ultra-chloe-neural/src/Neural.jsx
@@ -147,7 +147,7 @@ const Neural = () => {
   return (
     <div className="app">
       <video autoPlay muted loop id="bg-video">
-        <source src="ZoeSlowResPG13.mp4" type="video/mp4" />
+        <source src="ZoeVeryAngryLooped.mp4" type="video/mp4" />
       </video>
       <Dictaphone utterQuestion={utterQuestion} />
       <VoiceToText />
