8bf86c25ea68eab51d52a163e13a022299a7b51f
Daniel Smith <memorysight@gmail.com>
2024-07-29 19:28:26 -0400

    added PG13 Zoe

--- ultra-chloe-neural/src/Neural.jsx
+++ ultra-chloe-neural/src/Neural.jsx
@@ -147,7 +147,7 @@ const Neural = () => {
   return (
     <div className="app">
       <video autoPlay muted loop id="bg-video">
-        <source src="ZoeLipsStanding.mp4" type="video/mp4" />
+        <source src="ZoeSlowResPG13.mp4" type="video/mp4" />
       </video>
       <Dictaphone utterQuestion={utterQuestion} />
       <VoiceToText />
