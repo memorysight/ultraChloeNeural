e4f04aa3d6ceac82c876c6c73e04199c3461282a
Daniel Smith <memorysight@gmail.com>
2024-08-04 12:20:47 -0400

    best animation

--- ultra-chloe-neural/src/Neural.jsx
+++ ultra-chloe-neural/src/Neural.jsx
@@ -147,7 +147,7 @@ const Neural = () => {
   return (
     <div className="app">
       <video autoPlay muted loop id="bg-video">
-        <source src="ZoeStandingBkShort.mp4" type="video/mp4" />
+        <source src="ZoeBlueBestUse.mp4" type="video/mp4" />
       </video>
       <Dictaphone utterQuestion={utterQuestion} />
       <VoiceToText />
