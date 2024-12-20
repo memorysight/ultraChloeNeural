df88807b1d4c2d1de456bd5f3e21e9cbde3709f5
Daniel Smith <memorysight@gmail.com>
2024-12-20 13:29:35 -0500

    checking if push works

--- ultra-chloe-neural/src/Neural.jsx
+++ ultra-chloe-neural/src/Neural.jsx
@@ -7,7 +7,6 @@ import VoiceToText from './VoiceToText';
 import Overlay2 from './FutureUI/Overlay2';
 import './Neural.css';
 
-
 const Neural = () => {
   const [value, setValue] = useState('');
   const [error, setError] = useState('');
